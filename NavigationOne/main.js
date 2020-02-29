2f1ef98b0a82a316b8f9b571d924917dd6823563
Kentaro <hutran@outlook.com>
2020-02-29 23:30:18 +1100

    cleaned up code

--- NavigationOne/main.js
+++ NavigationOne/main.js
@@ -1,13 +1,3 @@
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2015, Codrops
- * http://www.codrops.com
- */
 ;(function(window) {
 
 	'use strict';
